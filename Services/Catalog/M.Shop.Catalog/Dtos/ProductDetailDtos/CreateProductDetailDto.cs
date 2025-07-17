bec3b9c6bbc8e20f9ed6d187b15319492f7b7340
Yunus Emre Binici <biniciyemre@gmail.com>
2025-07-17 15:58:19 +0300

    .

---
+++ Services/Catalog/M.Shop.Catalog/Dtos/ProductDetailDtos/CreateProductDetailDto.cs
@@ -0,0 +1,12 @@
+﻿namespace M.Shop.Catalog.Dtos.ProductDetailDtos
+{
+	public class CreateProductDetailDto
+	{
+		
+		public string ProductDescription { get; set; }
+
+		public string ProductID { get; set; }
+
+		public string ProductInformation { get; set; }
+	}
+}
---
+++ Services/Catalog/M.Shop.Catalog/Dtos/ProductDetailDtos/ResultProductDetailDto.cs
@@ -0,0 +1,13 @@
+﻿namespace M.Shop.Catalog.Dtos.ProductDetailDtos
+{
+	public class ResultProductDetailDto
+	{
+		public string ProductDetailID { get; set; }
+
+		public string ProductDescription { get; set; }
+
+		public string ProductID { get; set; }
+
+		public string ProductInformation { get; set; }
+	}
+}
