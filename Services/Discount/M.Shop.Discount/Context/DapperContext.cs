e0f1a0c2e2efec1cb00ecc79bc318ef79826e472
Yunus Emre Binici <biniciyemre@gmail.com>
2025-07-21 15:54:52 +0300

    .

---
+++ Services/Discount/M.Shop.Discount/Context/DapperContext.cs
@@ -0,0 +1,28 @@
+﻿using M.Shop.Discount.Entitites;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace M.Shop.Discount.Context
+{
+	public class DapperContext:DbContext
+	{
+		private readonly IConfiguration _configuration;
+		private readonly string _connectionString;
+
+		public DapperContext(IConfiguration configuration)
+		{
+			
+			_configuration = configuration;
+			_connectionString = _configuration.GetConnectionString("DefaultConnection");
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=EMRE\\SQLEXPRESS01; initial Catalog=MShopDiscountDb;integrated Security=true");
+
+		}
+		public DbSet<Coupon> Coupons { get; set; }
+		public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+	}
+}
---
+++ Services/Discount/M.Shop.Discount/Entitites/Coupon.cs
@@ -0,0 +1,15 @@
+﻿namespace M.Shop.Discount.Entitites
+{
+	public class Coupon
+	{
+		public int CouponId { get; set; }
+
+		public  string Code { get; set; }
+
+		public  bool IsActive { get; set; }
+
+		public int  Rate { get; set; }
+
+		public DateTime ValidDate { get; set; }
+	}
+}
