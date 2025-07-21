65a9359d57076f9fc13a69e5eff248771873d1f9
Yunus Emre Binici <biniciyemre@gmail.com>
2025-07-21 11:21:23 +0300

    .

---
+++ Services/Catalog/M.Shop.Catalog/Services/CategoryServices/CategoryServices.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using M.Shop.Catalog.Dtos.CategoryDtos;
+using M.Shop.Catalog.Entities;
+using M.Shop.Catalog.Settings;
+using MongoDB.Driver;
+
+namespace M.Shop.Catalog.Services.CategoryServices
+{
+	public class CategoryServices : ICategoryService
+	{
+		private readonly IMongoCollection<Category> _categoryCollection;
+		private readonly IMapper _mapper;
+
+		public CategoryServices(IMapper mapper,IDatabaseSettings _databaseSettings)
+		{
+			var client =new MongoClient(_databaseSettings.ConnectionString);
+			var database = client.GetDatabase(_databaseSettings.DatabaseName);
+			_categoryCollection = database.GetCollection<Category>(_databaseSettings.CategoryCollectionName);
+			_mapper = mapper;
+		}
+
+		public async Task CreateCategoryAsync(CreateCategoryDto categoryDto)
+		{
+			var value= _mapper.Map<Category>(categoryDto);
+			await _categoryCollection.InsertOneAsync(value);
+		}
+
+		public async Task DeleteCategoryAsync(string id)
+		{
+			await _categoryCollection.DeleteOneAsync(x=>x.CategoryID==id);
+		}
+
+		public async Task<ResultCategoryDto> GetByIdCategoryAsync(string id)
+		{
+			var values= await _categoryCollection.FindAsync(x=>x.CategoryID==id);
+			return _mapper.Map<ResultCategoryDto>(values);
+		}
+
+		public async Task<List<ResultCategoryDto>> GettAllCategoryAsync()
+		{
+			var values = await _categoryCollection.Find(x => true).ToListAsync();
+			return _mapper.Map<List<ResultCategoryDto>>(values);
+		}
+
+		public async Task UpdateCategoryAsync(UpdateCategoryDto categoryDto)
+		{
+			var value = _mapper.Map<Category>(categoryDto);
+			await _categoryCollection.FindOneAndReplaceAsync(x=>x.CategoryID==categoryDto.CategoryID,value);
+		}
+	}
+}
---
+++ Services/Catalog/M.Shop.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using M.Shop.Catalog.Dtos.CategoryDtos;
+
+namespace M.Shop.Catalog.Services.CategoryServices
+{
+	public interface ICategoryService
+	{
+		Task<List<ResultCategoryDto>> GettAllCategoryAsync();
+		Task CreateCategoryAsync(CreateCategoryDto categoryDto);
+		Task UpdateCategoryAsync(UpdateCategoryDto categoryDto);
+		Task DeleteCategoryAsync(string id);
+		Task <ResultCategoryDto>GetByIdCategoryAsync(string id);
+	}
+}
