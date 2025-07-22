d2476663278de9278f71005b5cd212e58c829e45
Yunus Emre Binici <biniciyemre@gmail.com>
2025-07-22 15:59:25 +0300

    .

--- Services/Order/Presentation/Order.WebApi/Program.cs
+++ Services/Order/Presentation/Order.WebApi/Program.cs
@@ -1,6 +1,31 @@
+using Application.Features.CQRS.Handlers.AddressHandlers;
+using Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using Application.Interfaces;
+using Application.Services;
+using Persistance.Repositories;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddApplicationService(builder.Configuration);
+
+#region 
+builder.Services.AddScoped<CreateAddressCommandHandler>();
+builder.Services.AddScoped<GetAddressByIdQueryHandler>();
+builder.Services.AddScoped<GetAddressQueryHandle>();
+builder.Services.AddScoped<RemoveAddressCommandHandle>();
+builder.Services.AddScoped<UpdateAddressCommandHandle>();
+
+builder.Services.AddScoped<CreateOrderDetailCommandHandle>();
+builder.Services.AddScoped<GetOrderDetailByIdQueryHandle>();
+builder.Services.AddScoped<GetOrderDetailQueryHandle>();
+builder.Services.AddScoped<RemoveOrderDetailCommandHandle>();
+builder.Services.AddScoped<UpdateOrderDetailCommandHandle>();
+#endregion
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
