c419fa2f21e36ed46b9e73a761f1b23e85a858a2
Yunus Emre Binici <biniciyemre@gmail.com>
2025-07-22 11:32:32 +0300

    .

---
+++ Services/Order/Core/Application/Features/CQRS/Commands/AddressComands/RemoveAddressCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CQRS.Commands.AddressComands
+{
+	public class RemoveAddressCommand
+	{
+		public int AddressId { get; set; }
+
+		public RemoveAddressCommand(int addressId)
+		{
+			AddressId = addressId;
+		}
+	}
+}
---
+++ Services/Order/Core/Application/Features/CQRS/Quaries/AddressQuaries/GetAddressByIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CQRS.Quaries.AddressQuaries
+{
+	public class GetAddressByIdQuery
+	{
+		public int Id { get; set; }
+
+		public GetAddressByIdQuery(int id)
+		{
+			Id = id;
+		}
+	}
+}
