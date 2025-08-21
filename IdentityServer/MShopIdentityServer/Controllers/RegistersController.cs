4a27562d1e9ec5b8525716a90652db8e59d9f15d
Yunus Emre Binici <biniciyemre@gmail.com>
2025-08-21 12:42:31 +0300

    .

---
+++ IdentityServer/MShopIdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MShopIdentityServer.Dtos;
+using MShopIdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MShopIdentityServer.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RegistersController : ControllerBase
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+
+		public RegistersController(UserManager<ApplicationUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+		{
+			var values = new ApplicationUser()
+			{
+				UserName = userRegisterDto.UserName,
+				Email = userRegisterDto.Email,
+				Surname = userRegisterDto.Surname,
+				Name = userRegisterDto.Name,
+
+			};
+			var result= await _userManager.CreateAsync(values,userRegisterDto.Password);
+			if (result.Succeeded)
+			{
+
+				return Ok("Kullanıcı Başarı İle Eklendi");
+			}
+			else {
+
+				return Ok("Hata Oluştu, tekrar deneyiniz");
+			}
+
+		}
+	}
+}
---
+++ IdentityServer/MShopIdentityServer/Dtos/UserRegisterDto.cs
@@ -0,0 +1,15 @@
+﻿namespace MShopIdentityServer.Dtos
+{
+	public class UserRegisterDto
+	{
+		public string UserName { get; set; }
+
+		public string Email { get; set; }
+
+		public string Name { get; set; }
+
+		public string Surname { get; set; }
+
+		public string Password { get; set; }
+	}
+}
