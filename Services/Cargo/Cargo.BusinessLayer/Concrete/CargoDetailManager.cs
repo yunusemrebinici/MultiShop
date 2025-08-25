67c3250642d2e0367cc6b2dc5f766b166dcf6d83
Yunus Emre Binici <biniciyemre@gmail.com>
2025-08-25 14:01:46 +0300

    .

---
+++ Services/Cargo/Cargo.BusinessLayer/Concrete/CargoDetailManager.cs
@@ -0,0 +1,46 @@
+﻿using Cargo.BusinessLayer.Abstract;
+using Cargo.DataAccessLayer.Abstract;
+using Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cargo.BusinessLayer.Concrete
+{
+	public class CargoDetailManager : ICargoDetailService
+	{
+		private readonly ICargoDetailDal _cargoDetailDal;
+
+		public CargoDetailManager(ICargoDetailDal cargoDetailDal)
+		{
+			_cargoDetailDal = cargoDetailDal;
+		}
+
+		public async Task TAdd(CargoDetail entity)
+		{
+			await _cargoDetailDal.Add(entity);
+		}
+
+		public async Task TDelete(CargoDetail entity)
+		{
+			await _cargoDetailDal.Update(entity);
+		}
+
+		public async Task<CargoDetail> TGetById(int id)
+		{
+			return await _cargoDetailDal.GetById(id);
+		}
+
+		public async Task<List<CargoDetail>> TGetListAll()
+		{
+			return await _cargoDetailDal.GetAll();
+		}
+
+		public async Task TUpdate(CargoDetail entity)
+		{
+			await _cargoDetailDal.Update(entity);
+		}
+	}
+}
