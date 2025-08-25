c99f453c7dfa611e3f364e62c241986947441802
Yunus Emre Binici <biniciyemre@gmail.com>
2025-08-25 14:30:52 +0300

    .

---
+++ Services/Cargo/Cargo.WebApi/Controllers/CargoCompaniesController.cs
@@ -0,0 +1,26 @@
+﻿using Cargo.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cargo.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CargoCompaniesController : ControllerBase
+	{
+		private readonly ICargoCompanyService _companyService;
+
+		public CargoCompaniesController(ICargoCompanyService companyService)
+		{
+			_companyService = companyService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAll()
+		{
+			return Ok(await _companyService.TGetListAll());
+		}
+
+		
+	}
+}
