7d3e1a676e48c2a982cc4b0905f6a915403127a1
Yunus Emre Binici <biniciyemre@gmail.com>
2025-08-25 14:13:50 +0300

    .

--- Services/Cargo/Cargo.WebApi/Program.cs
+++ Services/Cargo/Cargo.WebApi/Program.cs
@@ -1,6 +1,25 @@
+using Cargo.BusinessLayer.Abstract;
+using Cargo.BusinessLayer.Concrete;
+using Cargo.DataAccessLayer.Abstract;
+using Cargo.DataAccessLayer.Concrete;
+using Cargo.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<CargoContext>();
+
+builder.Services.AddScoped<ICargoCompanyDal,EfCargoComanyDal>();
+builder.Services.AddScoped<ICargoCustomerDal,EfCargoCustomerDal>();
+builder.Services.AddScoped<ICargoDetailDal,EfCargoDetailDal>();
+builder.Services.AddScoped<ICargoOperationDal,EfCargoOperationDal>();
+
+builder.Services.AddScoped<ICargoCompanyService,CargoCompanyManager>();
+builder.Services.AddScoped<ICargoCustomerService,CargoCustomerManager>();
+builder.Services.AddScoped<ICargoDetailService,CargoDetailManager>();
+builder.Services.AddScoped<ICargoOperationService,CargoOperationManager>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
