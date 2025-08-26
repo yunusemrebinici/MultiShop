2925a6db59ae9fa7ab3c746efbec195d98288cf2
Yunus Emre Binici <biniciyemre@gmail.com>
2025-08-26 17:33:18 +0300

    .

---
+++ Frontends/MShop.WebUI/ViewComponents/UILayoutViewComponents/_HeadUILayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+	public class _HeadUILayoutComponentPartial:ViewComponent
+	{
+		public async Task <IViewComponentResult> InvokeAsync()
+		{
+			return View();
+		}
+	}
+}
---
+++ Frontends/MShop.WebUI/ViewComponents/UILayoutViewComponents/_TopbarUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MShop.WebUI.ViewComponents.UILayoutViewComponents
+{
+	public class _TopbarUILayoutComponentPartial:ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View();
+		}
+	}
+}
