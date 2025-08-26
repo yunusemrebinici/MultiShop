dad5d85d96ac77bf0b407769941ecdc6aca598b4
Yunus Emre Binici <biniciyemre@gmail.com>
2025-08-26 18:21:56 +0300

    .

---
+++ Frontends/MShop.WebUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MShop.WebUI.ViewComponents.DefaultViewComponents
+{
+	public class _CarouselDefaultComponentPartial:ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View();
+		}
+	}
+}
---
+++ Frontends/MShop.WebUI/ViewComponents/DefaultViewComponents/_SpecialOfferComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MShop.WebUI.ViewComponents.DefaultViewComponents
+{
+	public class _SpecialOfferComponentPartial:ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View();
+		}
+	}
+}
