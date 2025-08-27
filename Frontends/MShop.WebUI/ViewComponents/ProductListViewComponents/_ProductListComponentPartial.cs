bfa498dc94e18a343f683e31bbcb45bdfb2ff881
Yunus Emre Binici <biniciyemre@gmail.com>
2025-08-27 16:28:43 +0300

    .

---
+++ Frontends/MShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MShop.WebUI.ViewComponents.ProductListViewComponents
+{
+	public class _ProductListComponentPartial:ViewComponent
+	{
+		public async Task <IViewComponentResult>InvokeAsync()
+		{
+			return View();
+		}
+	}
+}
---
+++ Frontends/MShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListGetProductCountComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MShop.WebUI.ViewComponents.ProductListViewComponents
+{
+	public class _ProductListGetProductCountComponentPartial:ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View();
+		}
+	}
+}
