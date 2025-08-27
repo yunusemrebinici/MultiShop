42124f1d9b362635cea30c6c6eec96275ee445cb
Yunus Emre Binici <biniciyemre@gmail.com>
2025-08-27 15:31:21 +0300

    .

---
+++ Frontends/MShop.WebUI/Controllers/ProductListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MShop.WebUI.Controllers
+{
+	public class ProductListController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
