684ce07f50f933a75ebfa55eb64eb1da58b58cab
Yunus Emre Binici <biniciyemre@gmail.com>
2025-08-28 16:25:52 +0300

    .

---
+++ Frontends/MShop.WebUI/ViewComponents/ShoppingCardViewComponents/_ShoppingCardListComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MShop.WebUI.ViewComponents.ShoppingCardViewComponents
+{
+	public class _ShoppingCardListComponentPartial:ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View();
+		}
+	}
+}
---
+++ Frontends/MShop.WebUI/ViewComponents/ShoppingCardViewComponents/_ShoppingCartDiscountCouponComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MShop.WebUI.ViewComponents.ShoppingCardViewComponents
+{
+	public class _ShoppingCartDiscountCouponComponentPartial:ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View();
+		}
+	}
+}
