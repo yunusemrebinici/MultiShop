6d3a6435d51700b1c5d6238972ddadaa2af8b881
Yunus Emre Binici <biniciyemre@gmail.com>
2025-08-28 17:47:33 +0300

    .

---
+++ Frontends/MShop.WebUI/Areas/Admin/Controllers/AdminLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MShop.WebUI.Areas.Admin.Controllers
+{
+	public class AdminLayoutController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Frontends/MShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MShop.WebUI.Areas.Admin.Controllers
+{
+	public class CategoryController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
