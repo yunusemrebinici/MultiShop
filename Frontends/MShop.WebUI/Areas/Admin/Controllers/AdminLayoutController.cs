3a7c7b6a96915b8dd88b37448f5b907e9a7b7037
Yunus Emre Binici <biniciyemre@gmail.com>
2025-08-28 17:57:08 +0300

    .

--- Frontends/MShop.WebUI/Areas/Admin/Controllers/AdminLayoutController.cs
+++ Frontends/MShop.WebUI/Areas/Admin/Controllers/AdminLayoutController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 namespace MShop.WebUI.Areas.Admin.Controllers
+
 {
+	[Area("Admin")]
+	[Route("Admin/[Controller]/[Action]")]
 	public class AdminLayoutController : Controller
 	{
 		public IActionResult Index()
