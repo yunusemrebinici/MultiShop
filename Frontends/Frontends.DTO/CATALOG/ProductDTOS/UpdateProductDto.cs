b60b63a5dd69d8c6a7e6cd0e18e93f7a6de9ecf0
Yunus Emre Binici <biniciyemre@gmail.com>
2025-08-29 20:52:49 +0300

    .

---
+++ Frontends/Frontends.DTO/CATALOG/ProductDTOS/UpdateProductDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Frontends.DTO.CATALOG.ProductDTOS
+{
+	public class UpdateProductDto
+	{
+	
+		public string ProductID { get; set; }
+
+		public string CategoryID { get; set; }
+
+		public string ProductName { get; set; }
+
+		public decimal ProductPrice { get; set; }
+
+		public string ProductImageUrl { get; set; }
+
+		public string ProductDescription { get; set; }
+	}
+}
---
+++ Frontends/MShop.WebUI/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,95 @@
+﻿
+using Frontends.DTO.CATALOG.ProductDTOS;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+namespace MShop.WebUI.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Route("Admin/[Controller]/[Action]")]
+	public class ProductController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public ProductController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7070/api/Products"); // API URL'si "Products" olarak değiştirildi.
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var json = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(json); // DTO'lar "Product" DTO'ları ile değiştirildi.
+				return View(values);
+			}
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> CreateProduct()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto) // DTO "CreateProductDto" olarak değiştirildi.
+		{
+			var client = _httpClientFactory.CreateClient();
+			var json = JsonConvert.SerializeObject(createProductDto);
+			StringContent stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:7070/api/Products", stringContent); // API URL'si "Products" olarak değiştirildi.
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+
+			return View();
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> UpdateProduct(string id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7070/api/Products/" + id); // API URL'si "Products" olarak değiştirildi.
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var json = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<UpdateProductDto>(json); // DTO "UpdateProductDto" olarak değiştirildi.
+				return View(values);
+			}
+
+			return View();
+		}
+
+		[HttpPost("{id}")]
+		public async Task<IActionResult> UpdateProduct(UpdateProductDto updateProductDto) // DTO "UpdateProductDto" olarak değiştirildi.
+		{
+			var client = _httpClientFactory.CreateClient();
+			var json = JsonConvert.SerializeObject(updateProductDto);
+			StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PutAsync("https://localhost:7070/api/Products", content); // API URL'si "Products" olarak değiştirildi.
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+
+			return View();
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> DeleteProduct(string id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.DeleteAsync("https://localhost:7070/api/Products/" + id); // API URL'si "Products" olarak değiştirildi.
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+	}
+}
