4bf2bc8c288ea4aacc31650280f239ef4d8f0a51
Yunus Emre Binici <biniciyemre@gmail.com>
2025-08-30 17:02:53 +0300

    .

--- Frontends/MShop.WebUI/Areas/Admin/Controllers/ProductController.cs
+++ Frontends/MShop.WebUI/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿
+using Frontends.DTO.CATALOG.CategoryDTOS;
 using Frontends.DTO.CATALOG.ProductDTOS;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using System.Text;
 namespace MShop.WebUI.Areas.Admin.Controllers
@@ -16,14 +18,37 @@ namespace MShop.WebUI.Areas.Admin.Controllers
 			_httpClientFactory = httpClientFactory;
 		}
 
+
+
+		internal async Task<List<SelectListItem>> CategorySelectListItem()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7070/api/Categories");
+			
+			
+				var json = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(json);
+				List<SelectListItem> selectListItems = (from x in values
+								   select new SelectListItem
+								   {
+									   Text = x.CategoryName,
+									   Value = x.CategoryID
+								   }).ToList();
+
+				return selectListItems;
+			
+			
+		}
+
+
 		public async Task<IActionResult> Index()
 		{
 			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync("https://localhost:7070/api/Products"); // API URL'si "Products" olarak değiştirildi.
+			var responseMessage = await client.GetAsync("https://localhost:7070/api/Products"); 
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				var json = await responseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(json); // DTO'lar "Product" DTO'ları ile değiştirildi.
+				var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(json); 
 				return View(values);
 			}
 			return View();
@@ -32,16 +57,19 @@ namespace MShop.WebUI.Areas.Admin.Controllers
 		[HttpGet]
 		public async Task<IActionResult> CreateProduct()
 		{
+			
+			ViewBag.CategoryNameList = CategorySelectListItem().Result;
+			
 			return View();
 		}
 
 		[HttpPost]
-		public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto) // DTO "CreateProductDto" olarak değiştirildi.
+		public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto) 
 		{
 			var client = _httpClientFactory.CreateClient();
 			var json = JsonConvert.SerializeObject(createProductDto);
 			StringContent stringContent = new StringContent(json, Encoding.UTF8, "application/json");
-			var responseMessage = await client.PostAsync("https://localhost:7070/api/Products", stringContent); // API URL'si "Products" olarak değiştirildi.
+			var responseMessage = await client.PostAsync("https://localhost:7070/api/Products", stringContent); 
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				return RedirectToAction("Index");
@@ -54,11 +82,12 @@ namespace MShop.WebUI.Areas.Admin.Controllers
 		public async Task<IActionResult> UpdateProduct(string id)
 		{
 			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync("https://localhost:7070/api/Products/" + id); // API URL'si "Products" olarak değiştirildi.
+			var responseMessage = await client.GetAsync("https://localhost:7070/api/Products/" + id); 
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				var json = await responseMessage.Content.ReadAsStringAsync();
-				var values = JsonConvert.DeserializeObject<UpdateProductDto>(json); // DTO "UpdateProductDto" olarak değiştirildi.
+				var values = JsonConvert.DeserializeObject<UpdateProductDto>(json);
+				ViewBag.CategoryNameListForUpdate = CategorySelectListItem().Result;
 				return View(values);
 			}
 
