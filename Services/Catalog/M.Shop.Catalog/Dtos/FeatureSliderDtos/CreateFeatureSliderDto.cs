fda416cbc4369544915c0e537845bb89b1848f74
Yunus Emre Binici <biniciyemre@gmail.com>
2025-09-02 14:51:16 +0300

    .

---
+++ Services/Catalog/M.Shop.Catalog/Dtos/FeatureSliderDtos/CreateFeatureSliderDto.cs
@@ -0,0 +1,14 @@
+﻿namespace M.Shop.Catalog.Dtos.FeatureSliderDtos
+{
+	public class CreateFeatureSliderDto
+	{
+		
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public string ImageUrl { get; set; }
+
+		public bool Status { get; set; }
+	}
+}
---
+++ Services/Catalog/M.Shop.Catalog/Dtos/FeatureSliderDtos/UpdateFeatureSliderDto.cs
@@ -0,0 +1,15 @@
+﻿namespace M.Shop.Catalog.Dtos.FeatureSliderDtos
+{
+	public class UpdateFeatureSliderDto
+	{
+		public string FeatureSliderId { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public string ImageUrl { get; set; }
+
+		public bool Status { get; set; }
+	}
+}
