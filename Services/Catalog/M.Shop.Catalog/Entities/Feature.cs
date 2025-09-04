bc03b65a2aed6c74794a73bbdcc90bc2585a3c64
Yunus Emre Binici <biniciyemre@gmail.com>
2025-09-04 17:39:28 +0300

    .

---
+++ Services/Catalog/M.Shop.Catalog/Entities/Feature.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace M.Shop.Catalog.Entities
+{
+	public class Feature
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId]
+		public string FeatureId { get; set; }
+
+		public string Icon { get; set; }
+
+		public string Title { get; set; }
+	}
+}
