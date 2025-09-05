923ff9c9b885d7952e6bb78070a705dc5b17e20f
Yunus Emre Binici <biniciyemre@gmail.com>
2025-09-05 18:43:31 +0300

    .

--- Frontends/MShop.WebUI/ViewComponents/DefaultViewComponents/_OfferDiscountDefaultComponentPartial.cs
+++ Frontends/MShop.WebUI/ViewComponents/DefaultViewComponents/_OfferDiscountDefaultComponentPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Frontends.DTO.CATALOG.OfferDiscountDTOS;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace MShop.WebUI.ViewComponents.DefaultViewComponents
 {
 	public class _OfferDiscountDefaultComponentPartial:ViewComponent
 	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _OfferDiscountDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
 		public async Task<IViewComponentResult> InvokeAsync()
 		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7070/api/OfferDiscounts");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var json = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultOfferDiscountDto>>(json);
+				return View(values);
+			}
 			return View();
 		}
 
