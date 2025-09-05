72dbc5b6f51ca666d726b8a5ad63ab3b82c26c2b
Yunus Emre Binici <biniciyemre@gmail.com>
2025-09-05 16:20:28 +0300

    .

--- Frontends/MShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
+++ Frontends/MShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Frontends.DTO.CATALOG.FeatureProductDTOS;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace MShop.WebUI.ViewComponents.DefaultViewComponents
 {
 	public class _FeatureProductsDefaultComponentPartial:ViewComponent
 	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _FeatureProductsDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
 
 		public async Task<IViewComponentResult> InvokeAsync()
 		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7070/api/FeatureProducts");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var json = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultFeatureProductDto>>(json);
+				return View(values);
+			}
 			return View();
 		}
 	}
