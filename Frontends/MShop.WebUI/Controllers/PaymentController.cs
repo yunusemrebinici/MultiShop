2f39ffc57a0bddd1297767ec20bc40680113bfc4
Yunus Emre Binici <biniciyemre@gmail.com>
2025-10-04 23:58:17 +0300

    .

--- Frontends/MShop.WebUI/Controllers/PaymentController.cs
+++ Frontends/MShop.WebUI/Controllers/PaymentController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Frontends.DTO.ORDER.Ordering;
+using Microsoft.AspNetCore.Mvc;
+using MShop.WebUI.Services.Interfaces;
+using MShop.WebUI.Services.OrderServices.Ordering;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace MShop.WebUI.Controllers
 {
 	public class PaymentController : Controller
 	{
+
+		private readonly IUserService _userService;
+		private readonly IOrderingService _orderingService;
+
+		public PaymentController(IOrderingService orderingService, IUserService userService)
+		{
+			
+			_orderingService = orderingService;
+			_userService = userService;
+		}
+
 		public IActionResult Index()
 		{
+			
 			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index(CreateOrderingDto createOrderingDto)
+		{
+			var userId = await _userService.GetUserInfo();
+			createOrderingDto.UserId = userId.Id;
+			await _orderingService.CreateOrdering(createOrderingDto);	
+			return RedirectToAction("Index","Order");
 		}
 
 
